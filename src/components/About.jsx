2311fbaa293c0384552cce5d61e6298750328f0b
Ichivin <144845734+Ichivin@users.noreply.github.com>
2026-01-19 15:14:42 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,25 +2,6 @@ import React from "react";
 import "../css/about.css";
 
 function About() {
-    return (
-        <div className="about">
-            <p className="about-text">
-                En tant que nouveau développeur web passionné par la création de solutions numériques innovantes, je
-                suis spécialisé dans les technologies HTML, CSS, JavaScript, React, Node.js, Firebase et MongoDB. Mon
-                parcours m'a permis d'acquérir une solide compréhension des principes fondamentaux du développement web,
-                ainsi que des compétences avancées dans la conception d'interfaces utilisateur attrayantes et réactives.
-            </p>
-            <p className="about-text">
-                Je m'efforce constamment d'approfondir mes connaissances et de rester à jour avec les dernières
-                tendances et technologies du secteur, afin de fournir des produits de haute qualité et adaptés aux
-                besoins spécifiques de mes clients. Si vous êtes intéressé par mon profil, veuillez prendre un moment
-                pour visiter mon portfolio, où vous trouverez des exemples concrets de certains de mes projets.
-            </p>
-            <p className="about-text">
-                N'hésitez pas à me contacter, je serais ravi de collaborer avec vous pour donner vie à vos projets.
-            </p>
-        </div>
-    );
   return (
     <div className="about">
       <p className="about-text">
@@ -48,3 +29,4 @@ function About() {
 }
 
 export default About;
+
